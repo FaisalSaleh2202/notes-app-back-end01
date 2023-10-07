4427415ce76c6892d7fcb8035e72a985e1feb15d
FaisalSaleh2202 <faisal.saleh127@gmail.com>
2023-10-07 17:43:18 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-console */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
